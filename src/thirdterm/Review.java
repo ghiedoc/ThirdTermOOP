ed5fc1c81df3f0c08cf3e65d5fe5921cf33ec54f
Gillian Joanna Doctor <ghiedoc@gmail.com>
2019-02-11 19:38:07 +0800

    practice

--- src/thirdterm/Review.java
+++ src/thirdterm/Review.java
@@ -1,6 +1,11 @@
 package thirdterm;
 
 //Recursion
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashSet;
+
 public class Review {
 
     public int convert(int n) {
@@ -57,6 +62,66 @@ public class Review {
         //System.out.println(String.valueOf(array_a));
         return String.valueOf(array_a);
     }
+    
+    public int totalUnique(String filePath){
+        
+        //The dog barks The cat meow meow cat dog
+        HashSet<String> setList = new HashSet<>();
+        try {
+            BufferedReader read = new BufferedReader(new FileReader(filePath));
+            String line = null;
+            while ((line = read.readLine()) != null) {
+
+                String[] wordArray = line.split(" ");
+                
+                for (int index = 0; index < wordArray.length; index++) {
+                    setList.add(wordArray[index]);
+                }
+            }
+
+        } catch (Exception e) {
+        }
+
+
+        return setList.size();
+        
+    }
+    
+    /**
+     * TO-DO code here the logic that checks if the specified element exists in
+     * the given array or not.
+     *
+     * @param list the list of string from user.
+     * @param find the element that will be search from the list
+     *
+     * @return True if the element exists, otherwise false.
+     *
+     */
+    public static boolean isInsideElement(String[] list, String find) {
+        
+        for(int i = 0; i < list.length; i++){
+            if(find.equals(list[i])){
+                String found = list[i];
+                System.out.println(found);
+                return true;
+            }
+        }
+        System.out.println();
+        
+        return false;
+    }
+    
+    /**
+     * TO-DO code here the logic that displays all the unique words in the list
+     * and its corresponding frequency.
+     *
+     * @param list the list of string from user.
+     */
+    public void displayUniqueWithFrequency(String[] list) {
+
+        
+        
+    }
 
     public static void main(String[] args) {
         Review r = new Review();
@@ -65,6 +130,10 @@ public class Review {
         //System.out.println(r.permute("AAB", 0, 0));
         //System.out.println(r.recurPermute("AAB", "A"));
         //r.recurPermute("AAB", "A");
-        permutation("", "ABA");
+        //permutation("", "ABA");
+        String[] arr = {"dog", "cat", "rabbit"};
+        System.out.println(r.isInsideElement(arr, "aso"));
+        
+        System.out.println(r.totalUnique("C:\\Users\\Gillian\\Documents\\NetBeansProjects\\OOP-master\\src\\test\\files\\TestThree.txt"));
     }
 }
